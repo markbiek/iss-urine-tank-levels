2bc79b7180e03e80a7154bc2c864017267c60d02
Mark Biek <support@janustech.net>
2013-11-19 10:04:14 -0500

    Added readme

--- main.py
+++ main.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python
 
-"""
-Simple python script which attempts to read the Urine Tank level of the ISS and post it to Twitter.
-
-Since the ISS website loads its data asynchronously, we use the Selenium Webdriver module to launch 
-and invisible Firefox instance to load into the page.
-
-This requires a headless X-server. We're using Xvfb (http://en.wikipedia.org/wiki/Xvfb).
-
-How to launch Xvfb:
-    Xvfb :1 &
-    export DISPLAY=:1
-
-This Stackoverflow question (http://stackoverflow.com/a/4474362/305) has a great example of how to
-generate Twitter access tokens and interactive with Twitter.
-
-Latest Twitter api module (which uses v1.1) is on github https://github.com/bear/python-twitter
-"""
-
 from HTMLParser import HTMLParser
 from BeautifulSoup import BeautifulSoup
 from selenium import webdriver
